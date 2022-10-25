89ad18f9b6328e23a89a408e43bf11e69bb8e461
peihsinchang <peihsinchang@eztravel.com.tw>
2022-10-25 10:37:49 +0800

    Updates

---
+++ precache-manifest.78b80ef352e65f108ec73fa295d36564.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "38399f6924b62b5cdc1d9eecdca0b668",
+    "url": "./index.html"
+  },
+  {
+    "revision": "08b57ce6fd432eccfe5b",
+    "url": "./static/css/main.1b9bcb54.chunk.css"
+  },
+  {
+    "revision": "7abf040fb3b6bba24213",
+    "url": "./static/js/2.aa30364f.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "./static/js/2.aa30364f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "08b57ce6fd432eccfe5b",
+    "url": "./static/js/main.1edb8cfa.chunk.js"
+  },
+  {
+    "revision": "c7f0defd51a5df21de17",
+    "url": "./static/js/runtime-main.9cf16503.js"
+  }
+]);
